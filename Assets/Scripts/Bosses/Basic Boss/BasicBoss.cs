ec7cc730f888f00b0a2899147fb7f3c8e17e4a56
Callum <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-11-17 11:28:45 +1300

    Boss Beginnings
    - Created health component
    - Created projectile script
    - Create basic boss script (WIP)

---
+++ Assets/Scripts/Bosses/Basic Boss/BasicBoss.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBoss : MonoBehaviour
+{
+    private enum EState
+    {
+        idle,
+        attacking,
+    };
+    private EState m_state = EState.idle;
+
+    private float m_fireRate = 1.0f;
+
+    private void Update()
+    {
+        switch (m_state)
+        {
+            case EState.idle:
+                {
+                    Idle();
+                    break;
+                }
+
+            case EState.attacking:
+                {
+                    Attacking();
+                    break;
+                }
+
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+    // Called every frame the boss is idle
+    private void Idle()
+    {
+        m_state = EState.attacking;
+    }
+
+    // Called every frame the boss is attacking
+    private void Attacking()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Bosses/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    private float m_speed = 1.0f;
+    private Rigidbody m_rigidBody = null;
+
+    private Action m_onHit = null;
+    private Type[] m_hitTypes = null;
+
+    private void Awake()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Initialises the projectile
+    public void Init(Action _onHit, Type[] _hitTypes, float _speed = 1.0f)
+    {
+        m_speed = _speed;
+
+        m_onHit = _onHit;
+
+        m_hitTypes = _hitTypes;
+    }
+
+    // Checks if the collision was with something valid
+    private void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < m_hitTypes.Length; i++)
+        {
+            Component comp = other.GetComponent(m_hitTypes[i]);
+            if (comp)
+            {
+                m_onHit?.Invoke();
+                return;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        m_rigidBody.AddForce(transform.forward * m_speed, ForceMode.Impulse);
+    }
+}

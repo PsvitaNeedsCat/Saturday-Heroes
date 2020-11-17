d97ca417b791c0b6e13687f2033130286311e18c
Tim <timbbby@gmail.com>
2020-11-17 17:35:50 +1300

    Billboard Script

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform m_billboardedObject;
+
+    private Camera m_cam;
+
+    private void Awake()
+    {
+        m_cam = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        m_billboardedObject.LookAt(m_billboardedObject.position + m_cam.transform.rotation * Vector3.forward, m_cam.transform.rotation * Vector3.up);
+    }
+}
